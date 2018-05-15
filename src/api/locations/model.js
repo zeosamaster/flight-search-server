f7715a0cdd101a03ebd176079589a67038a6b2e0
zeosamaster <fabio_mcm2@hotmail.com>
2018-05-15 19:55:30 +0100

    locations model

--- src/api/locations/model.js
+++ src/api/locations/model.js
@@ -1,58 +1,78 @@
 import mongoose, { Schema } from "mongoose";
 
 const locationsSchema = new Schema(
-  {
-    Code: {
-      type: String
+    {
+        Code: {
+            type: String
+        },
+        Name: {
+            type: String
+        },
+        CoordsX: {
+            type: Number
+        },
+        CoordsY: {
+            type: Number
+        },
+        CityId: {
+            type: String
+        },
+        CityName: {
+            type: String
+        },
+        CountryId: {
+            type: String
+        },
+        CountryName: {
+            type: String
+        },
+        ContinentId: {
+            type: String
+        },
+        ContinentName: {
+            type: String
+        }
     },
-    Name: {
-      type: String
-    },
-    CoordsX: {
-      type: Number
-    },
-    CoordsY: {
-      type: Number
-    },
-    CityId: {
-      type: String
-    },
-    CityName: {
-      type: String
-    },
-    CountryId: {
-      type: String
-    },
-    CountryName: {
-      type: String
-    },
-    ContinentId: {
-      type: String
-    },
-    ContinentName: {
-      type: String
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+            transform: (obj, ret) => {
+                delete ret._id;
+            }
+        }
     }
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-      transform: (obj, ret) => {
-        delete ret._id;
-      }
-    }
-  }
 );
 
 locationsSchema.methods = {
-  view(full) {
-    return full
-      ? {
-          ...this
-          // add properties for a full view
+    view(full = this) {
+        let continent = {
+            id: full.ContinentId,
+            name: full.ContinentName
+        };
+        let country = {
+            id: full.CountryId,
+            name: full.CountryName,
+            continent
+        };
+        let city = {
+            id: full.CityId,
+            name: full.CityName,
+            country
+        };
+        let coords = {
+            y: full.CoordsY,
+            x: full.CoordsX
+        };
+        let airport = {
+            id: full.Id,
+            code: full.Code,
+            name: full.Name,
+            coords,
+            city
         }
-      : this;
-  }
+        return airport;
+    }
 };
 
 const model = mongoose.model("Locations", locationsSchema);
